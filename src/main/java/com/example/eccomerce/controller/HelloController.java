6c69d50268e65cc4d783f5c99f989caf16250d68
Rafael Abednego <rafaelabednego123@gmail.com>
2025-08-19 23:45:45 +0700

    init

---
+++ src/main/java/com/example/eccomerce/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.eccomerce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World from Ecommerce API!";
+    }
+}
